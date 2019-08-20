ee678d50a103c6bfc4fbadb896888348ff6fbf86
PsyhoZOOM <psyhozoom@gmail.com>
2019-08-20 14:55:22 +0200

    Removed Skeleton App

--- config/modules.config.php
+++ config/modules.config.php
@@ -23,6 +23,5 @@ return [
     'Zend\Db',
     'Zend\Router',
     'Zend\Validator',
-  'Application',
     'FFMpeg'
 ];
--- module/FFMpeg/config/module.config.php
+++ module/FFMpeg/config/module.config.php
@@ -124,9 +124,59 @@ return [
             ],
         ],
     ],
-    'view_manager'  => [
+    'view_manager'  =>  [
+        'display_not_found_reason' => true,
+        'display_exceptions'       => true,
+        'doctype'                  => 'HTML5',
+        'not_found_template'       => 'error/404',
+        'exception_template'       => 'error/index',
+        'template_map' => [
+            'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
+            'application/index/index' => __DIR__ . '/../view/application/index/index.phtml',
+            'error/404'               => __DIR__ . '/../view/error/404.phtml',
+            'error/index'             => __DIR__ . '/../view/error/index.phtml',
+        ],
         'template_path_stack' => [
             __DIR__ . '/../view',
+        ],
+        
+    ],
+     'navigation' => [
+        'default' => [
+            [
+                'label' => 'FFMpeg',
+                'route' => 'ffmpeg',
+                'pages' => [
+                    [
+                        'label'     => 'Add',
+                        'route'     => 'ffmpeg',
+                        'action'    => 'addstream',
+                    ],
+                    [
+                        'label'     => 'Edit',
+                        'route'     => 'ffmpeg',
+                        'action'    => 'editstream',
+                    ],
+                    [
+                        'label'     => "Delete",
+                        'route'     => 'ffmpeg',
+                        'action'    => 'deletestream',
+                    ],
+                    [
+                        'label'     => 'List of Out streams',
+                        'route'     => 'ffmpeg/streamout',
+                        'action'    => 'showstreams',
+                        'pages'     => [
+                            [
+                                'label'     => 'Edit Stream Out',
+                                'route'     => 'ffmpeg/streamout/editstreamouts[/:id]',
+                                'action'    => 'editstreamout',
+                            ],
+                        ],
+                    ],
+
+                ],
+            ],
         ],
     ],
 
